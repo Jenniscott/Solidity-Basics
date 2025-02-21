794a6706fd6cab862dd635daf444675f49c31bfd
Jenniscott <109913440+Jenniscott@users.noreply.github.com>
2025-02-21 01:39:50 +0000

    Day 2 -- Event

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,6 +5,10 @@ contract SimpleStorage {
     uint256 public storedNumber;
     address public owner;
 
+
+    event NumberStored(address indexed user, uint256 number);
+    event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
+
     constructor () {
         owner = msg.sender;
     }
@@ -16,6 +20,8 @@ contract SimpleStorage {
 
     function storeNumber(uint256 _number) public onlyOwner {
         storedNumber = _number;
+
+        emit NumberStored(msg.sender, _number);
     }
 
     function retrieveNumber() public view returns (uint256) {
@@ -24,7 +30,10 @@ contract SimpleStorage {
 
     function transferOwnership(address _newOwner) public onlyOwner {
         require(_newOwner != address(0), "Address cannot be zero");
+        address oldOwner = owner;
         owner = _newOwner;
+
+        emit OwnershipTransferred(oldOwner, _newOwner);
     }
 
 }
