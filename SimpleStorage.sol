739c91f559e47cff1f6eccb152d320ab56b86c81
Jenniscott <109913440+Jenniscott@users.noreply.github.com>
2025-02-22 22:42:01 +0000

    Day 3 - Mapping

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.19;
 contract SimpleStorage {
     uint256 public storedNumber;
     address public owner;
+    mapping(address => uint256) public userNumber;
 
-
-    event NumberStored(address indexed user, uint256 number);
+    event UserNumberStored(address indexed user, uint256 number);
     event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
+    
 
     constructor () {
         owner = msg.sender;
@@ -18,15 +19,16 @@ contract SimpleStorage {
         _;
     }
 
-    function storeNumber(uint256 _number) public onlyOwner {
-        storedNumber = _number;
+    function storeNumber(uint256 _number) public  {
+        userNumber[msg.sender] = _number;
 
-        emit NumberStored(msg.sender, _number);
+        emit UserNumberStored(msg.sender, _number);
     }
 
     function retrieveNumber() public view returns (uint256) {
-        return storedNumber;
+        return userNumber[msg.sender];
     }
+
 
     function transferOwnership(address _newOwner) public onlyOwner {
         require(_newOwner != address(0), "Address cannot be zero");
@@ -37,3 +39,4 @@ contract SimpleStorage {
     }
 
 }
+
